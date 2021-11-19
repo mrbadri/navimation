40662f1dd6137ec0bd5d24bfb4f667a7dce325b5
mohammad badri <mohammad.b@arianalabs.com>
2021-11-19 21:47:46 +0330

    cleane code navimation.ts

--- src/navimation.ts
+++ src/navimation.ts
@@ -3,10 +3,8 @@ import { IInput } from "./types"
 import { inputValidation, inputDefault } from "./utils";
 
 export function Navimation(input: IInput): void {
-
     if (inputValidation(input)) {
         input = inputDefault(input);
-        // console.log(input);
         controller(input);
     }
 }
